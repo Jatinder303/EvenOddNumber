ce70152d8e585037a15894ab9a92de227d2e649a
Jatinder Singh <Jatinder.Singh@atcltd.atc.org.nz>
2019-05-21 10:11:21 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOddNumber
+{
+    class Program
+    {
+        // This program will find the number is even or odd
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number = ");
+            int num = int.Parse(Console.ReadLine());
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("This is an even number");
+            }
+            else
+            {
+                Console.WriteLine("This is an odd number");
+            }
+        }
+    }
+}
